3fdba1c60a698864f762618a3072a6084be53ee5
l4dn0 <ves.artem2005@gmail.com>
2024-12-06 12:03:45 +0300

    Задание 13

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace exp2
         /// <param name="min">Минимальное значение диапазона.</param>
         /// <param name="max">Максимальное значение диапазона.</param>
         /// <returns>Произведение значений f, удовлетворяющих условию.</returns>
-        public static double CalculateInOneDimentional(InitFunction func, List<int> T, double min, double max)
+        public static double CalculateInOneDimentional(IBaseFunction func, List<int> T, double min, double max)
         {
             double product = 1;
             foreach (double element in T)
@@ -53,7 +53,7 @@ namespace exp2
             BaseFunction1 baseFunc = new();
             double productF1 = CalculateInOneDimentional(baseFunc, T, min, max);
             ExecuteFile.SetResultToFile("../../../writefile.txt", $"Произведение значений f1 в диапазоне [{min}, {max}]: {productF1}");
-            
+
             // Второй вызов с использованием функции f2
             BaseFunction2 derivedFunc = new();
             double productF2 = CalculateInOneDimentional(derivedFunc, T, min, max);
@@ -62,44 +62,50 @@ namespace exp2
     }
 
     /// <summary>
-    /// Абстрактный класс, инициализирующий метод f
+    /// Интерфейс - инициализатор функции f
     /// </summary>
-    public abstract class InitFunction
+    interface IBaseFunction
     {
-        public abstract double f(double x);
+        /// <summary>
+        /// инициализировать метод f
+        /// </summary>
+        /// <param name="x">принимаемое аргумент - число x</param>
+        /// <returns></returns>
+        double f(double x);
     }
 
     /// <summary>
     /// Первый базовый класс, реализация метода f = f1
     /// </summary>
-    public class BaseFunction1 : InitFunction
-    {
+    public class BaseFunction1 : IBaseFunction
+    { 
         /// <summary>
         /// Переопределённый метод f = f1
         /// </summary>
         /// <param name="x">Аргумент функции</param>
         /// <returns>Квадрат числа x</returns>
-        public override double f(double x)
+        public double f(double x)
         {
-            return x * x;
+            return Math.Pow(x, x);
         }
     }
 
     /// <summary>
     /// Второй базовый класс, реализация метода f = f2
     /// </summary>
-    public class BaseFunction2 : InitFunction
+    public class BaseFunction2 : IBaseFunction
     {
         /// <summary>
         /// Переопределённый метод f = f2
         /// </summary>
         /// <param name="x"></param>
         /// <returns>Инкрементированное число x</returns>
-        public override double f(double x)
+        public double f(double x)
         {
-            return x + 1;
+            return Math.Log(x);
         }
     }
+
 
 
     /// <summary>
@@ -140,4 +146,5 @@ namespace exp2
             sw.Close();
         }
     }
+
 }
